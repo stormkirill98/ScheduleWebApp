570b7b15558f7ed30dd4d9450315bbe1067d0c0d
stormkirill98 <stormkirill98@gmail.com>
2020-03-11 12:48:44 +0300

    setup authntithication with fake backend

--- ScheduleWebApp/ClientApp/src/app/app.routing.ts
+++ ScheduleWebApp/ClientApp/src/app/app.routing.ts
@@ -4,14 +4,16 @@ import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 import { AuthGuard } from './_guards';
+import {NavMenuComponent} from './nav-menu/nav-menu.component';
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'fetch-data', component: NavMenuComponent },
 
-    // otherwise redirect to home
-    { path: '**', redirectTo: '' }
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
--- ScheduleWebApp/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ScheduleWebApp/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Observable } from 'rxjs';
+import {AuthenticationService} from '../_services';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
   isExpanded = false;
+  isLogged: Observable<boolean>;
+
+  constructor(private authService: AuthenticationService) {  }
+
+  ngOnInit() {
+    this.isLogged = this.authService.isLoggedIn;
+  }
 
   collapse() {
     this.isExpanded = false;
