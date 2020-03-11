601294dc00cc41eb859b4ee005ad913491aa9e30
stormkirill98 <stormkirill98@gmail.com>
2020-03-11 14:56:20 +0300

    auth is work

--- ScheduleWebApp/ClientApp/src/app/app.module.ts
+++ ScheduleWebApp/ClientApp/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { routing } from './app.routing';
 
-import { fakeBackendProvider } from './_helpers';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AlertService, AuthenticationService, UserService } from './_services';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -12,8 +13,6 @@ import { HomeComponent } from './home';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AlertComponent } from './_directives';
 import { AuthGuard } from './_guards';
-import { JwtInterceptor, ErrorInterceptor } from './_helpers';
-import { AlertService, AuthenticationService, UserService } from './_services';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 
@@ -40,9 +39,7 @@ import { RegisterComponent } from './register';
     UserService,
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-    // provider used to create fake backend
-    fakeBackendProvider],
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
