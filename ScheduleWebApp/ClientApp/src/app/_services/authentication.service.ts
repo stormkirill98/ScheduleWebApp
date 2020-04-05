217c84091ac38ea3d6ff10dc116c5941c6fe462d
stormkirill98 <stormkirill98@gmail.com>
2020-04-05 17:54:10 +0300

    rework to hide nav tabs

--- ScheduleWebApp/ClientApp/src/app/_services/authentication.service.ts
+++ ScheduleWebApp/ClientApp/src/app/_services/authentication.service.ts
@@ -1,20 +1,12 @@
-﻿import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import {BehaviorSubject} from 'rxjs';
-import {environment} from '../../environments/environment';
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { User } from '../_models';
 
 @Injectable()
 export class AuthenticationService {
-  private loggedIn: BehaviorSubject<boolean>;
-  userRole: string;
-
-  get isLoggedIn() {
-    return this.loggedIn.asObservable();
-  }
-
   constructor(private http: HttpClient) {
-    this.loggedIn = new BehaviorSubject<boolean>(!!localStorage.getItem('currentUser'));
   }
 
   login(username: string, password: string) {
@@ -24,8 +16,6 @@ export class AuthenticationService {
         if (user && user.token) {
           // store user details and jwt token in local storage to keep user logged in between page refreshes
           localStorage.setItem('currentUser', JSON.stringify(user));
-          this.loggedIn.next(true);
-          this.userRole = user.role;
         }
 
         return user;
@@ -35,18 +25,21 @@ export class AuthenticationService {
   logout() {
     // remove user from local storage to log user out
     localStorage.removeItem('currentUser');
-    this.loggedIn.next(false);
   }
 
   isDirector(): boolean {
-    return this.userRole === 'Директор';
+    return this.getCurrentUser().role === 'Директор';
   }
 
   isTeacher(): boolean {
-    return this.userRole === 'Учитель';
+    return this.getCurrentUser().role === 'Учитель';
   }
 
   isStudent(): boolean {
-    return this.userRole === 'Студент';
+    return this.getCurrentUser().role === 'Студент';
+  }
+
+  getCurrentUser(): User {
+    return JSON.parse(localStorage.getItem('currentUser'));
   }
 }
